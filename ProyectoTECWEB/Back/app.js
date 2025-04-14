6bbbb3faaf0706ba6cfacde18ae55a1079ecf7a6
SaiPlusSai <refgu774@gmail.com>
2025-04-14 02:40:35 -0400

    Documentos
    Documentos

--- ProyectoTECWEB/Back/app.js
+++ ProyectoTECWEB/Back/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import usuariosRoutes from './routes/usuarios.js';
+import documentosRoutes from './routes/documentos.js';
 
 const app = express();
+
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // <--- ESTA LÍNEA ES VITAL
 
 app.use('/api/usuarios', usuariosRoutes);
-
+app.use('/api/documentos', documentosRoutes);
 export default app;
